9a64e2b95bb011b4eb0b963bdb423335f766b443
lee-jiwon <jiwon.lee@mdstech.co.kr>
2023-01-17 16:29:29 +0900

    [feat] 비디오 목록 호출 기능 구현

--- app/src/main/java/com/project/toyyoutube/MainActivity.kt
+++ app/src/main/java/com/project/toyyoutube/MainActivity.kt
@@ -2,6 +2,14 @@ package com.project.toyyoutube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.project.toyyoutube.dto.VideoDto
+import com.project.toyyoutube.service.VideoService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +19,36 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, PlayerFragment())
             .commit()
+
+        getVideoList()
+    }
+
+    private fun getVideoList() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(VideoService::class.java).also {
+
+            it.listVideos()
+                .enqueue(object : Callback<VideoDto> {
+                    override fun onResponse(call: Call<VideoDto>, response: Response<VideoDto>) {
+                        if (response.isSuccessful.not()) {
+                            Log.d("MainActivity", "response fail")
+                            return
+                        }
+
+                        response.body()?.let {
+                            Log.d("MainActivity", it.toString())
+                        }
+                    }
+
+                    override fun onFailure(call: Call<VideoDto>, t: Throwable) {
+                        Log.d("MainActivity", "onFailure ${t.toString()}")
+                        // ...
+                    }
+                })
+        }
     }
 }
--- app/src/main/java/com/project/toyyoutube/PlayerFragment.kt
+++ app/src/main/java/com/project/toyyoutube/PlayerFragment.kt
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment
 import com.project.toyyoutube.databinding.FragmentPlayerBinding
 import kotlin.math.abs
 
+/**
+ * Video JSON
+ * https://run.mocky.io/v3/9f56da3e-7b5b-4a36-8e1a-c49aaeed3d82
+ */
 class PlayerFragment : Fragment(R.layout.fragment_player) {
 
     private var binding: FragmentPlayerBinding? = null
